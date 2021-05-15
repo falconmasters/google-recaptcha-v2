beafc9143e824f19558611219d54a2e919a45a43
Carlos <carlos@falconmasters.com>
2021-05-15 15:18:13 -0500

    Agregando archivos iniciales

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	return (
+		<div className="contenedor">
+			<div className="registrate">
+				<h1>Registrate</h1>
+				<form className="formulario" action="" onSubmit={submit}>
+					<input type="text" name="usuario" id="usuario" placeholder="Usuario" />
+					<input type="password" name="password" id="password" placeholder="Contraseña" />
+					<input type="password" name="password2" id="password2" placeholder="Repetir Contraseña" />
+					<div className="recaptcha">
+					</div>
+					<button type="submit">Iniciar Sesion</button>
+				</form>
+			</div>
+			<div>
+				<h1>Bienvenido</h1>
+			</div>
+		</div>
+	);
 }
-
+ 
 export default App;
